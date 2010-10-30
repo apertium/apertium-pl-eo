b46f2e36d284df62ed48d988b64b43c7dae715f8
Jim O'Regan <joregan@gmail.com>
2010-10-30 22:38:24 +0000

    basic conversion

---
+++ dev/revo.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+binmode STDIN, ":utf8";
+#binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+my $pos;
+
+sub getpos {
+	if (/e$/) {
+		return 'adv';
+	} elsif (/o$/) {
+		return 'n';
+	} elsif (/a$/) {
+		return 'adj';
+	} elsif (/i$/) {
+		return 'vblex';
+	} elsif (/e\!$/) {
+		return 'adv';
+	} elsif (/\!$/) {
+		return 'ij';
+	} else {
+		return '<UNK>';
+	}
+}
+
+sub outword ($$$$) {
+	my ($l, $r, $pos, $whole) = @_;
+	$pos = getpos($r);
+	if ($pos eq "n" && $r =~ /^\p{Lu}/) {
+		$pos = "np";
+	}
+	if ($l =~ s/\!$// || $pos eq 'ij' || $pos eq '<UNK>') {
+		$pos = "ij";
+	}
+	if ($pos eq '<UNK>') {
+		print "    <!-- $whole -->\n"
+#		return;
+	} 
+
+	my $l1 = $l;
+	my $l2 = $l;
+	my $r1 = $r;
+	my $r2 = $r;
+
+	if ($l !~ /[\(\)]/ && $r !~ /[\(\)]/) {
+		print "    <e><p><l>$l<s n=\"$pos\"/></l><r>$r<s n=\"$pos\"/></r></p></e>\n";
+		return;
+	} elsif ($l =~ /[\(\)]/ && $r !~ /[\(\)]/) {
+		$l1 =~ s/\([^\)]*\) ?//;
+		$l2 =~ s/[\(\)]//g;
+		print "    <e><p><l>$l1<s n=\"$pos\"/></l><r>$r<s n=\"$pos\"/></r></p></e>\n";
+		print "    <e r=\"LR\"><p><l>$l2<s n=\"$pos\"/></l><r>$r<s n=\"$pos\"/></r></p></e>\n";
+		return;
+	} elsif ($l !~ /[\(\)]/ && $r =~ /[\(\)]/) {
+		$r1 =~ s/\([^\)]*\) ?//;
+		$r2 =~ s/[\(\)]//g;
+		print "    <e><p><l>$l<s n=\"$pos\"/></l><r>$r1<s n=\"$pos\"/></r></p></e>\n";
+		print "    <e r=\"RL\"><p><l>$l<s n=\"$pos\"/></l><r>$r2<s n=\"$pos\"/></r></p></e>\n";
+		return;
+	} else {
+		$l1 =~ s/\([^\)]*\) ?//;
+		$r1 =~ s/\([^\)]*\) ?//;
+		$l2 =~ s/[\(\)]//g;
+		$r2 =~ s/[\(\)]//g;
+		print "    <e><p><l>$l1<s n=\"$pos\"/></l><r>$r1<s n=\"$pos\"/></r></p></e>\n";
+		print "    <e r=\"LR\"><p><l>$l2<s n=\"$pos\"/></l><r>$r1<s n=\"$pos\"/></r></p></e>\n";
+		print "    <e r=\"RL\"><p><l>$l1<s n=\"$pos\"/></l><r>$r2<s n=\"$pos\"/></r></p></e>\n";
+		return;
+	}
+	print "    <!-- $whole -->\n"
+#	return;	
+}
+
+while (<>)
+{
+	chomp;
+	my ($l, $r) = split /: ?/;
+	next if (!$r || $r eq '');
+	$l =~ s/^ *//;
+	$l =~ s/ *$//;
+	$r =~ s/,$//;
+	$r =~ s/^ *//;
+	$r =~ s/ *$//;
+	if ($r !~ /[;,]/) {
+		outword ($l, $r, $pos, $_);
+	} else {
+		foreach my $ind (split /[;,] ?/, $r) {
+			outword ($l, $ind, $pos, $_);
+		}
+	}
+}
